6da597c76e056e9ec7c80b1fdcbdd77b1a0afe40
hwangsea <134906042+hwangsea@users.noreply.github.com>
2025-03-26 13:53:54 +0900

    [기능추가][1,2단계] Application 시작점 구현

---
+++ src/main/java/Application.java
@@ -0,0 +1,15 @@
+import controller.LottoController;
+import domain.LottoNumberGenerator;
+import domain.NumbersGenerator;
+import view.InputView;
+import view.ResultView;
+
+public class Application {
+  public static void main(String[] args) {
+    InputView inputView = new InputView();
+    ResultView resultView = new ResultView();
+    NumbersGenerator numbersGenerator = new LottoNumberGenerator();
+    LottoController lottoController = new LottoController(inputView, resultView, numbersGenerator);
+    lottoController.run();
+  }
+}
