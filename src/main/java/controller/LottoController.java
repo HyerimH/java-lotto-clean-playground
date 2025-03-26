6b47008c678ded72377d0b46a5be27fa28c706c3
hwangsea <134906042+hwangsea@users.noreply.github.com>
2025-03-26 13:53:23 +0900

    [기능추가][1,2단계] LottoController 기본 로직 구현

---
+++ src/main/java/controller/LottoController.java
@@ -0,0 +1,40 @@
+package controller;
+
+import domain.Lottos;
+import domain.Money;
+import domain.NumbersGenerator;
+import domain.Statistics;
+import domain.WinningNumbers;
+import view.InputView;
+import view.ResultView;
+
+public class LottoController {
+
+  private final InputView inputView;
+  private final ResultView resultView;
+  private final NumbersGenerator numbersGenerator;
+
+  public LottoController(InputView inputView, ResultView resultView, NumbersGenerator numbersGenerator) {
+    this.inputView = inputView;
+    this.resultView = resultView;
+    this.numbersGenerator = numbersGenerator;
+  }
+
+  public void run() {
+    // 구매 금액 처리
+    resultView.printPurchaseMessage();
+    Money money = new Money(inputView.getInt());
+
+    // 로또 생성 및 출력
+    Lottos lottos = Lottos.createLottos(money.calculateTicketCount(), numbersGenerator);
+    resultView.printLottos(lottos);
+
+    // 당첨 번호 입력
+    resultView.printWinningNumber();
+    WinningNumbers winningNumbers = inputView.getWinningNumbers();
+
+    // 당첨 결과 출력
+    Statistics statistics = new Statistics(lottos, winningNumbers, money);
+    resultView.printStatistics(statistics);
+  }
+}
