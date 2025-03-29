d75309de535e8c1bf36c5eab7d9e613034e99596
hwangsea <134906042+hwangsea@users.noreply.github.com>
2025-03-29 20:27:37 +0900

    [기능개선][1,2단계] 리포맷 코드

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -5,6 +5,7 @@ import view.InputView;
 import view.ResultView;
 
 public class Application {
+
   public static void main(String[] args) {
     InputView inputView = new InputView();
     ResultView resultView = new ResultView();
