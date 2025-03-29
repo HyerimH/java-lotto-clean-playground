ae7fee981c91b1c16c2f4834c3d922585e92ec01
hwangsea <134906042+hwangsea@users.noreply.github.com>
2025-03-29 23:43:45 +0900

    [기능개선][1,2단계] getTicketCount 메서드 추가

--- src/main/java/domain/Lottos.java
+++ src/main/java/domain/Lottos.java
@@ -9,19 +9,25 @@ public class Lottos {
   private final List<Lotto> lottos;
   private final Integer ticketCount;
 
-  private Lottos(List<Lotto> lottos) {
+  private Lottos(List<Lotto> lottos, int ticketCount) {
     this.lottos = new ArrayList<>(lottos);
+    this.ticketCount = ticketCount;
   }
 
-  public static Lottos createLottos(int amount, NumbersGenerator numbersGenerator) {
+  public static Lottos createLottos(Money money, NumbersGenerator numbersGenerator) {
+    Integer ticketCount = money.getPurchaseMoney() / TICKET_PRICE;
     List<Lotto> lottoList = new ArrayList<>();
-    for (int i = 0; i < amount; i++) {
-      lottoList.add(new Lotto(numbersGenerator));
+    for (int i = 0; i < ticketCount; i++) {
+      lottoList.add(Lotto.createAuto(numbersGenerator));
     }
-    return new Lottos(lottoList);
+    return new Lottos(lottoList, ticketCount);
   }
 
   public List<Lotto> getLottos() {
     return new ArrayList<>(lottos);
+  }
+
+  public Integer getTicketCount() {
+    return ticketCount;
   }
 }
