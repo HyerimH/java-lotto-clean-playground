77e6d2db752729eccf2423c5f58c3111794e7506
hwangsea <134906042+hwangsea@users.noreply.github.com>
2025-03-31 01:57:25 +0900

    [기능개선][1,2단계] LottoNumberGenerator에 생성자 추가

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,4 +1,5 @@
 import controller.LottoController;
+import domain.Lotto;
 import domain.LottoNumberGenerator;
 import domain.NumbersGenerator;
 import view.InputView;
@@ -9,7 +10,7 @@ public class Application {
   public static void main(String[] args) {
     InputView inputView = new InputView();
     ResultView resultView = new ResultView();
-    NumbersGenerator numbersGenerator = new LottoNumberGenerator();
+    NumbersGenerator numbersGenerator = new LottoNumberGenerator(Lotto.MIN_NUMBER, Lotto.MAX_NUMBER, Lotto.LOTTO_SIZE);
     LottoController lottoController = new LottoController(inputView, resultView, numbersGenerator);
     lottoController.run();
   }
--- src/main/java/domain/LottoNumberGenerator.java
+++ src/main/java/domain/LottoNumberGenerator.java
@@ -8,19 +8,29 @@ import java.util.stream.IntStream;
 
 public class LottoNumberGenerator implements NumbersGenerator {
 
-  private static final List<Integer> BASE_NUMBERS = createBaseNumbers();
+  private final int minNumber;
+  private final int maxNumber;
+  private final int size;
+  private final List<Integer> baseNumbers;
 
-  private static List<Integer> createBaseNumbers() {
-    return IntStream.rangeClosed(Lotto.MIN_NUMBER, Lotto.MAX_NUMBER)
+  public LottoNumberGenerator(int minNumber, int maxNumber, int size) {
+    this.minNumber = minNumber;
+    this.maxNumber = maxNumber;
+    this.size = size;
+    this.baseNumbers = createBaseNumbers();
+  }
+
+  private List<Integer> createBaseNumbers() {
+    return IntStream.rangeClosed(minNumber, maxNumber)
         .boxed()
         .collect(Collectors.collectingAndThen(Collectors.toList(), Collections::unmodifiableList));
   }
 
   @Override
   public List<Integer> generate() {
-    List<Integer> baseNumbers = new ArrayList<>(BASE_NUMBERS);
+    List<Integer> baseNumbers = new ArrayList<>(this.baseNumbers);
     Collections.shuffle(baseNumbers);
-    List<Integer> result = baseNumbers.subList(0, Lotto.LOTTO_SIZE);
+    List<Integer> result = baseNumbers.subList(0, size);
     result.sort(Integer::compareTo);
     return new ArrayList<>(result);
   }
