ca9c7a701667db7df648f22068a98c6502f88c5b
hwangsea <134906042+hwangsea@users.noreply.github.com>
2025-04-02 20:31:05 +0900

    [기능추가][3,4,5단계] NumbersGenerator.java 인터페이스 제거

--- src/main/java/domain/LottoNumberGenerator.java
+++ src/main/java/domain/LottoNumberGenerator.java
@@ -3,33 +3,27 @@ package domain;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.Random;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
-public class LottoNumberGenerator implements NumbersGenerator {
+public class LottoNumberGenerator{
 
-  private final int minNumber;
-  private final int maxNumber;
-  private final int size;
-  private final List<Integer> baseNumbers;
+  private final Random random;
 
-  public LottoNumberGenerator(int minNumber, int maxNumber, int size) {
-    this.minNumber = minNumber;
-    this.maxNumber = maxNumber;
-    this.size = size;
-    this.baseNumbers = createBaseNumbers();
+  public LottoNumberGenerator(Random random) {
+    this.random = random;
   }
 
-  private List<Integer> createBaseNumbers() {
-    return IntStream.rangeClosed(minNumber, maxNumber)
+  public LottoNumberGenerator() {
+    this(new Random());
+  }
+
+  public List<Integer> generate(int minNumber, int maxNumber, int size) {
+    List<Integer> baseNumbers = IntStream.rangeClosed(minNumber, maxNumber)
         .boxed()
-        .collect(Collectors.collectingAndThen(Collectors.toList(), Collections::unmodifiableList));
-  }
-
-  @Override
-  public List<Integer> generate() {
-    List<Integer> baseNumbers = new ArrayList<>(this.baseNumbers);
-    Collections.shuffle(baseNumbers);
+        .collect(Collectors.toList());
+    Collections.shuffle(baseNumbers, random);
     List<Integer> result = baseNumbers.subList(0, size);
     result.sort(Integer::compareTo);
     return new ArrayList<>(result);
