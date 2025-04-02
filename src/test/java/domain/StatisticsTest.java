ae8ec9c22c600bdfda56bacaeb237602a2b75f82
hwangsea <134906042+hwangsea@users.noreply.github.com>
2025-04-02 20:32:58 +0900

    [기능수정][3,4,5단계] StatisticsTest 수정

--- src/test/java/domain/StatisticsTest.java
+++ src/test/java/domain/StatisticsTest.java
@@ -2,6 +2,7 @@ package domain;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
+import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
 import org.junit.jupiter.api.DisplayName;
@@ -10,47 +11,45 @@ import org.junit.jupiter.api.Test;
 class StatisticsTest {
 
   @Test
-  @DisplayName("로또 번호와 당첨 번호를 비교하여 당첨 등수를 제대로 계산한다")
+  @DisplayName("로또 번호와 당첨 번호를 비교하여 당첨 등수를 제대로 계산하고 상금과 수익률을 계산한다.")
   void calculateMatchCounts() {
     // Given
-    int[][] testNumbers = {
-        {1, 2, 3, 4, 5, 6},
-        {1, 2, 3, 4, 5, 7},
-        {1, 2, 3, 4, 7, 8},
-        {1, 2, 3, 7, 8, 9},
-        {1, 2, 7, 8, 9, 10},
-        {1, 7, 8, 9, 10, 11},
-        {7, 8, 9, 10, 11, 12}
-    };
-    NumbersGenerator numbersGenerator = new TestNumberGenerator(testNumbers);
-    Money money = new Money(7000);
-    Lottos lottos = Lottos.createLottos(money, numbersGenerator);
-    WinningNumbers winningNumbers = new WinningNumbers(List.of(1, 2, 3, 4, 5, 6));
+    List<Lotto> lottoList = Arrays.asList(
+        Lotto.createManual(Arrays.asList(1, 2, 3, 4, 5, 6)),
+        Lotto.createManual(Arrays.asList(1, 2, 3, 4, 5, 7)),
+        Lotto.createManual(Arrays.asList(1, 2, 3, 4, 5, 9)),
+        Lotto.createManual(Arrays.asList(1, 2, 3, 4, 8, 9)),
+        Lotto.createManual(Arrays.asList(1, 2, 3, 9, 10, 11)),
+        Lotto.createManual(Arrays.asList(1, 2, 9, 10, 11, 12)),
+        Lotto.createManual(Arrays.asList(1, 9, 10, 11, 12, 13))
+    );
+    Lottos lottos = new Lottos(lottoList, 7, 7, new Money(7000));
+    WinningNumbers winningNumbers = new WinningNumbers(Arrays.asList(1, 2, 3, 4, 5, 6));
+    BonusNumber bonusNumber = new BonusNumber(7, winningNumbers);
     Money investedMoney = new Money(7000);
-
-    Statistics statistics = new Statistics(lottos, winningNumbers, investedMoney);
+    Statistics statistics = new Statistics(lottos, winningNumbers, investedMoney, bonusNumber);
 
     // When
     Map<WinningRank, Integer> matchCounts = statistics.calculateMatchCounts();
     long totalPrize = statistics.calculateTotalPrize(matchCounts);
     double profitRate = statistics.calculateProfitRate(matchCounts);
 
-    long expectedPrize =
-        (WinningRank.SIX.getPrize() * 1) +
-        (WinningRank.FIVE.getPrize() * 1) +
-        (WinningRank.FOUR.getPrize() * 1) +
-        (WinningRank.THREE.getPrize() * 1);
-
-    assertThat(totalPrize).isEqualTo(expectedPrize);
-
-    double expectedProfitRate = (double) expectedPrize / investedMoney.getPurchaseMoney();
-
     // Then
     assertThat(matchCounts.get(WinningRank.SIX)).isEqualTo(1);
+    assertThat(matchCounts.get(WinningRank.FIVE_WITH_BONUS)).isEqualTo(1);
     assertThat(matchCounts.get(WinningRank.FIVE)).isEqualTo(1);
     assertThat(matchCounts.get(WinningRank.FOUR)).isEqualTo(1);
     assertThat(matchCounts.get(WinningRank.THREE)).isEqualTo(1);
-    assertThat(matchCounts.get(WinningRank.NONE)).isEqualTo(3);
-    assertThat(profitRate).isEqualTo(expectedProfitRate);
+    assertThat(matchCounts.get(WinningRank.NONE)).isEqualTo(2);
+
+    assertThat(totalPrize).isEqualTo(
+        WinningRank.SIX.getPrize() +
+        WinningRank.FIVE_WITH_BONUS.getPrize() +
+        WinningRank.FIVE.getPrize() +
+        WinningRank.FOUR.getPrize() +
+        WinningRank.THREE.getPrize()
+    );
+
+    assertThat(profitRate).isEqualTo((double) totalPrize / investedMoney.getPurchaseMoney());
   }
 }
