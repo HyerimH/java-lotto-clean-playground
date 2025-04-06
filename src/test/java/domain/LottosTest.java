fbf419e02e5062873eca2b9e80d86dd190212392
hwangsea <134906042+hwangsea@users.noreply.github.com>
2025-04-06 21:20:26 +0900

    [기능추가][3,4,5단계] 수동티켓 음수 검증 로직 추가

--- src/test/java/domain/LottosTest.java
+++ src/test/java/domain/LottosTest.java
@@ -1,8 +1,10 @@
 package domain;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
 
 import java.util.ArrayList;
+import java.util.List;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -12,11 +14,11 @@ class LottosTest {
   @DisplayName("구입 금액 5500원으로 티켓을 생성할 때, 5개의 티켓만 생성된다")
   void createLottosWith5500() {
     // Given
-    int purchaseMoney = 5500;
+    long purchaseMoney = 5500;
     LottoNumberGenerator lottoNumberGenerator = new LottoNumberGenerator();
 
     // When
-    Lottos lottos = Lottos.from(purchaseMoney, new ArrayList<>(), lottoNumberGenerator);
+    Lottos lottos = Lottos.from(purchaseMoney, new ArrayList<>(), 0,lottoNumberGenerator);
 
     // Then
     assertThat(lottos.getTicketCount()).isEqualTo(5);
@@ -27,14 +29,31 @@ class LottosTest {
   @DisplayName("구입 금액 6000원으로 티켓을 생성할 때, 6개의 티켓이 생성된다")
   void createLottosWith6000() {
     // Given
-    int purchaseMoney = 6000;
+    long purchaseMoney = 6000;
     LottoNumberGenerator lottoNumberGenerator = new LottoNumberGenerator();
 
     // When
-    Lottos lottos = Lottos.from(purchaseMoney, new ArrayList<>(), lottoNumberGenerator);
+    Lottos lottos = Lottos.from(purchaseMoney, new ArrayList<>(), 0, lottoNumberGenerator);
 
     // Then
     assertThat(lottos.getTicketCount()).isEqualTo(6);
     assertThat(lottos.getLottos()).hasSize(6);
+  }
+
+  @Test
+  @DisplayName("수동 티켓 수가 음수일 경우 예외가 발생한다.")
+  void manualTicketCountCannotNegative() {
+    // Given
+    long purchaseMoney = 6000;
+    LottoNumberGenerator lottoNumberGenerator = new LottoNumberGenerator();
+
+    List<Lotto> manualLottos = new ArrayList<>();
+    manualLottos.add(Lotto.createManual(List.of(new LottoNumber(1), new LottoNumber(2),
+        new LottoNumber(3), new LottoNumber(4), new LottoNumber(5), new LottoNumber(6))));
+
+    // When & Then
+    assertThatThrownBy(() -> Lottos.from(purchaseMoney, manualLottos, -1, lottoNumberGenerator))
+        .isInstanceOf(IllegalArgumentException.class)
+        .hasMessage("수동 구매 수는 음수일 수 없습니다.");
   }
 }
